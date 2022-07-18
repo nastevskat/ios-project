7853d788adcf1c6a2570bf4b3d807e9b59b8df85
Tamara Nastevska <nastevskat@yahoo.com>
2022-07-18 22:45:50 +0200

    changes

--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -7,34 +7,30 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
     
-    var taps: Int = 0
+    private var taps: Int = 0
 
+  
+  
     @IBOutlet weak var btnOutlet: UIButton!
     @IBOutlet weak var hello: UILabel!
     
-    @IBAction func btn(_ sender: Any) {
+    
+    @IBAction func buttonPressed() {
         taps += 1
         hello.text = String(taps)
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         btnOutlet.layer.cornerRadius = 35
-        // Do any additional setup after loading the view.
+       
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
