9435ef65b8fd013763f2036ca966c34f39570809
Tamara Nastevska <nastevskat@yahoo.com>
2022-07-20 20:46:18 +0200

    Login Layout + Splash Screen + App Icon

--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -1,36 +1,37 @@
-//
-//  LoginViewController.swift
-//  TV Shows
-//
-//  Created by Infinum on 13/07/2022.
-//
 
 import UIKit
-
-final class LoginViewController: UIViewController {
+extension UITextField{
     
-    private var taps: Int = 0
-
-  
-  
-    @IBOutlet weak var btnOutlet: UIButton!
-    @IBOutlet weak var hello: UILabel!
-    
-    @IBAction private func buttonPressed() {
-        taps += 1
-        hello.text = String(taps)
-        
+    func setBottomBorder(){
+        self.layer.shadowColor = UIColor.darkGray.cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        self.layer.shadowOpacity = 1.0
+        self.layer.shadowRadius = 0.0
     }
+}
+
+class LoginViewController: UIViewController{
     
+
+    @IBOutlet weak var LoginBtn: UIButton!
+    
+    @IBOutlet weak var emailOutlet: UITextField!
+    
+    @IBOutlet weak var PassOutlet: UITextField!
+    
+    @IBOutlet weak var Remember: UIButton!
+    
+    @IBOutlet weak var RegisterBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        LoginBtn.layer.cornerRadius = 20
 
-        btnOutlet.layer.cornerRadius = 35
+    
        
     }
     
-
-   
-
+    
 }
+
